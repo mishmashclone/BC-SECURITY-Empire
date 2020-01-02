1c1bcd8c6498f3dbe7b8ac5d232e0917b929d6a2
Vince Rose <vrose04@gmail.com>
2020-01-01 21:49:22 -0700

    take out extra else

--- setup/install.sh
+++ setup/install.sh
@@ -139,48 +139,42 @@ if uname | grep -q "Darwin"; then
 	sudo pip install -r requirements.txt --global-option=build_ext \
 		--global-option="-L/usr/local/opt/openssl/lib" \
 		--global-option="-I/usr/local/opt/openssl/include"
-	# In order to build dependencies these should be exproted.
+	# In order to build dependencies these should be exported.
 	export LDFLAGS=-L/usr/local/opt/openssl/lib
 	export CPPFLAGS=-I/usr/local/opt/openssl/include
-else
-	version=$( lsb_release -r | grep -oP "[0-9]+" | head -1 )
-	if lsb_release -d | grep -q "Fedora"; then
-		Release=Fedora
-		sudo dnf install -y make automake gcc gcc-c++  python-devel m2crypto python-m2ext swig libxml2-devel java-openjdk-headless openssl-devel openssl libffi-devel redhat-rpm-config
-		sudo pip install -r requirements.txt
-	elif lsb_release -d | grep -q "Kali"; then
-		Release=Kali
-		apt-get update
-		sudo apt-get install -y make g++ python-dev python-m2crypto swig python-pip libxml2-dev default-jdk zlib1g-dev libssl1.1 build-essential libssl-dev libxml2-dev zlib1g-dev
-		sudo pip install -r requirements.txt
-		install_powershell
-	elif lsb_release -d | grep -q "Ubuntu"; then
-		Release=Ubuntu
-		sudo apt-get update
-		if [ is_libssl_1_0 ]; then
-				LibSSL_pkgs="libssl1.0.0 libssl-dev"
-				Pip_file="requirements_libssl1.0.txt"
-		else
-				LibSSL_pkgs="libssl1.1 libssl-dev"
-				Pip_file="requirements.txt"
-		fi
-		sudo apt-get install -y make g++ python-dev python-m2crypto swig python-pip libxml2-dev default-jdk $LibSSL_pkgs build-essential
-		sudo pip install -r $Pip_file
-		install_powershell
+elif lsb_release -d | grep -q "Fedora"; then
+	sudo dnf install -y make automake gcc gcc-c++  python-devel m2crypto python-m2ext swig libxml2-devel java-openjdk-headless openssl-devel openssl libffi-devel redhat-rpm-config
+	sudo pip install -r requirements.txt
+elif lsb_release -d | grep -q "Kali"; then
+	apt-get update
+	sudo apt-get install -y make g++ python-dev python-m2crypto swig python-pip libxml2-dev default-jdk zlib1g-dev libssl1.1 build-essential libssl-dev libxml2-dev zlib1g-dev
+	sudo pip install -r requirements.txt
+	install_powershell
+elif lsb_release -d | grep -q "Ubuntu"; then
+	sudo apt-get update
+	if [ is_libssl_1_0 ]; then
+			LibSSL_pkgs="libssl1.0.0 libssl-dev"
+			Pip_file="requirements_libssl1.0.txt"
 	else
-		echo "Unknown distro - Debian/Ubuntu Fallback"
-		sudo apt-get update
-		if [ is_libssl_1_0 ]; then
-				LibSSL_pkgs="libssl1.0.0 libssl-dev"
-				Pip_file="requirements_libssl1.0.txt"
-		else
-				LibSSL_pkgs="libssl1.1 libssl-dev"
-				Pip_file="requirements.txt"
-		fi
-		sudo apt-get install -y make g++ python-dev python-m2crypto swig python-pip libxml2-dev default-jdk libffi-dev $LibSSL_pkgs build-essential
-		sudo pip install -r $Pip_file
-		install_powershell
+			LibSSL_pkgs="libssl1.1 libssl-dev"
+			Pip_file="requirements.txt"
 	fi
+	sudo apt-get install -y make g++ python-dev python-m2crypto swig python-pip libxml2-dev default-jdk $LibSSL_pkgs build-essential
+	sudo pip install -r $Pip_file
+	install_powershell
+else
+	echo "Unknown distro - Debian/Ubuntu Fallback"
+	sudo apt-get update
+	if [ is_libssl_1_0 ]; then
+			LibSSL_pkgs="libssl1.0.0 libssl-dev"
+			Pip_file="requirements_libssl1.0.txt"
+	else
+			LibSSL_pkgs="libssl1.1 libssl-dev"
+			Pip_file="requirements.txt"
+	fi
+	sudo apt-get install -y make g++ python-dev python-m2crypto swig python-pip libxml2-dev default-jdk libffi-dev $LibSSL_pkgs build-essential
+	sudo pip install -r $Pip_file
+	install_powershell
 fi
 
 install_xar

29da1cb53eefc2be482fa995a0b861c541ac73c0
Hubbl3 <Jake.Krasnov@BC-Security.org>
2020-01-07 22:54:35 -0600

    removed unsupported invoke-shellcode option

--- lib/modules/powershell/code_execution/invoke_shellcode.py
+++ lib/modules/powershell/code_execution/invoke_shellcode.py
@@ -58,11 +58,7 @@ class Module(object):
                 'Required'      :   False,
                 'Value'         :   ''            
             },
-            'Payload' : {
-                'Description'   :   'Metasploit payload to inject (reverse_http[s]).',
-                'Required'      :   False,
-                'Value'         :   'reverse_http'
-            },
+
             'Lhost' : {
                 'Description'   :   'Local host handler for the meterpreter shell.',
                 'Required'      :   False,
@@ -75,7 +71,7 @@ class Module(object):
             },
             'Shellcode' : {
                 'Description'   :   'Custom shellcode to inject, 0xaa,0xab,... format.',
-                'Required'      :   False,
+                'Required'      :   True,
                 'Value'         :   ''
             }
         }

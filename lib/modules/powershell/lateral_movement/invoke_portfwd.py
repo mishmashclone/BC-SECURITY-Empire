d147ab6260f58c570f343f323ef55dd58e2ea3aa
Sam Freeside <snovvcrash@protonmail.ch>
2020-08-07 08:52:00 +0300

    Add port forwarding module (#265)
    * Add port forwarding module
    
    * Update references
    
    * Add call to the keyword obfuscation function

---
+++ lib/modules/powershell/lateral_movement/invoke_portfwd.py
@@ -0,0 +1,161 @@
+from __future__ import print_function
+from builtins import str
+from builtins import object
+from lib.common import helpers
+
+
+class Module(object):
+
+    def __init__(self, mainMenu, params=[]):
+
+        # Metadata info about the module, not modified during runtime
+        self.info = {
+            # Name for the module that will appear in module menus
+            'Name': 'Invoke-PortFwd',
+
+            # List of one or more authors for the module
+            'Author': ['@decoder-it'],
+
+            # More verbose multi-line description of the module
+            'Description': ('Forward a port with no admin rights required.'),
+
+            'Software': '',
+
+            'Techniques': ['T1363'],
+
+            # True if the module needs to run in the background
+            'Background': True,
+
+            # File extension to save the file as
+            'OutputExtension': None,
+
+            # True if the module needs admin rights to run
+            'NeedsAdmin': False,
+
+            # True if the method doesn't touch disk/is reasonably opsec safe
+            'OpsecSafe': False,
+
+            # The language for this module
+            'Language': 'powershell',
+
+            # The minimum PowerShell version needed for the module to run
+            'MinLanguageVersion': '2',
+
+            # List of any references/other comments
+            'Comments': [
+                'Inspired by @IppSec (https://youtu.be/6l4ZIKwzW8U)',
+                'https://github.com/decoder-it/psportfwd'
+            ]
+        }
+
+        # Any options needed by the module, settable during runtime
+        self.options = {
+            # Format:
+            #   value_name : {description, required, default_value}
+            'Agent': {
+                # The 'Agent' option is the only one that MUST be in a module
+                'Description':   'Agent to grab a screenshot from.',
+                'Required'   :   True,
+                'Value'      :   ''
+            },
+            'Lhost': {
+                'Description':   'Local IP address.',
+                'Required'   :   True,
+                'Value'      :   ''
+            },
+            'Lport': {
+                'Description':   'Local port to listen on.',
+                'Required'   :   True,
+                'Value'      :   ''
+            },
+            'Rhost': {
+                'Description':   'Remote IP address.',
+                'Required'   :   True,
+                'Value'      :   ''
+            },
+            'Rport': {
+                'Description':   'Remote port to forward to.',
+                'Required'   :   True,
+                'Value'      :   ''
+            }
+        }
+
+        # Save off a copy of the mainMenu object to access external
+        #   functionality like listeners/agent handlers/etc.
+        self.mainMenu = mainMenu
+
+        # During instantiation, any settable option parameters are passed as
+        #   an object set to the module and the options dictionary is
+        #   automatically set. This is mostly in case options are passed on
+        #   the command line.
+        if params:
+            for param in params:
+                # Parameter format is [Name, Value]
+                option, value = param
+                if option in self.options:
+                    self.options[option]['Value'] = value
+
+
+    def generate(self, obfuscate=False, obfuscationCommand=""):
+
+        # The PowerShell script itself, with the command to invoke for
+        #   execution appended to the end. Scripts should output everything
+        #   to the pipeline for proper parsing.
+        #
+        # If you're planning on storing your script in module_source as a ps1,
+        #   or if you're importing a shared module_source, use the first
+        #   method to import it and the second to add any additional code and
+        #   launch it.
+        #
+        # If you're just going to inline your script, you can delete the first
+        #   method entirely and just use the second. The script should be
+        #   stripped of comments, with a link to any original reference script
+        #   included in the comments.
+        #
+        # First method: Read in the source script from module_source
+        moduleSource = self.mainMenu.installPath + "/data/module_source/lateral_movement/Invoke-PortFwd.ps1"
+        if obfuscate:
+            helpers.obfuscate_module(moduleSource=moduleSource, obfuscationCommand=obfuscationCommand)
+            moduleSource = moduleSource.replace("module_source", "obfuscated_module_source")
+        try:
+            f = open(moduleSource, 'r')
+        except:
+            print(helpers.color("[!] Could not read module source path at: " + str(moduleSource)))
+            return ""
+
+        moduleCode = f.read()
+        f.close()
+
+        # If you'd just like to import a subset of the functions from the
+        #   module source, use the following:
+        #   script = helpers.generate_dynamic_powershell_script(moduleCode, ["Get-Something", "Set-Something"])
+        script = moduleCode
+
+        # Second method: For calling your imported source, or holding your
+        #   inlined script. If you're importing source using the first method,
+        #   ensure that you append to the script variable rather than set.
+        #
+        # The script should be stripped of comments, with a link to any
+        #   original reference script included in the comments.
+        #
+        # If your script is more than a few lines, it's probably best to use
+        #   the first method to source it.
+        #
+        # script += """
+        scriptEnd = "Invoke-PortFwd"
+
+        # Add any arguments to the end execution of the script
+        for option, values in self.options.items():
+            if option.lower() != "agent":
+                if values['Value'] and values['Value'] != '':
+                    if values['Value'].lower() == "true":
+                        # if we're just adding a switch
+                        scriptEnd += " -" + str(option)
+                    else:
+                        scriptEnd += " -" + str(option) + " " + str(values['Value'])
+        if obfuscate:
+            scriptEnd = helpers.obfuscate(psScript=scriptEnd, installPath=self.mainMenu.installPath, obfuscationCommand=obfuscationCommand)
+        script += scriptEnd
+        script = helpers.keyword_obfuscation(script)
+
+        return script

97a79f7e1119542c78ab14de0cb45c8ede2fc08f
Cx01N <anthony.rose@bc-security.org>
2020-09-02 00:47:00 -0400

    Updated version and changelog to 3.4.0RC-1

--- lib/common/empire.py
+++ lib/common/empire.py
@@ -15,7 +15,7 @@ from builtins import input
 from builtins import range
 from builtins import str
 
-VERSION = "3.3.4 BC-Security Fork"
+VERSION = "3.4.0-RC1 BC Security Fork"
 
 from pydispatch import dispatcher
 

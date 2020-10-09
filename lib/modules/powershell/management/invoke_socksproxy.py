021d2011db5cccd25706370d2f21bab8d4fcfa5c
hubbl3 <jake.krasnov@bc-security.org>
2020-10-08 21:52:42 -0500

    minor fixes

--- lib/modules/powershell/management/invoke_socksproxy.py
+++ lib/modules/powershell/management/invoke_socksproxy.py
@@ -42,7 +42,7 @@ class Module(object):
 
         self.options = {
             'Agent': {
-                'Description': 'Agent to grab a screenshot from.',
+                'Description': 'Agent to run module on.',
                 'Required': True,
                 'Value': ''
             },

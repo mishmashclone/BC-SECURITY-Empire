86e88f95a46af3695d271efb0fa0437822117044
Anthony Rose <20302208+Cx01N@users.noreply.github.com>
2021-03-08 20:21:24 -0800

    added Start-ProcessAsUser module (#54)

---
+++ empire/server/modules/powershell/management/start-processasuser.py
@@ -0,0 +1,125 @@
+from __future__ import print_function
+
+from builtins import object
+from builtins import str
+
+from empire.server.common import helpers
+
+
+class Module(object):
+
+    def __init__(self, mainMenu, params=[]):
+
+        self.info = {
+            'Name': 'Start-ProcessAsUser',
+
+            'Author': ['@mattifestation', '@tifkin_'],
+
+            'Description': ('Executes a command using a specified set of credentials.'),
+
+            'Software': '',
+
+            'Techniques': ['T1088'],
+
+            'Background' : False,
+
+            'OutputExtension' : None,
+            
+            'NeedsAdmin' : False,
+
+            'OpsecSafe' : True,
+            
+            'Language' : 'powershell',
+
+            'MinLanguageVersion' : '2',
+            
+            'Comments': [
+                'https://github.com/leechristensen/Random/blob/master/PowerShellScripts/Start-ProcessAsUser.ps1'
+            ]
+        }
+
+        # any options needed by the module, settable during runtime
+        self.options = {
+            # format:
+            #   value_name : {description, required, default_value}
+            'Agent' : {
+                'Description'   :   'Agent to run module on.',
+                'Required'      :   True,
+                'Value'         :   ''
+            },
+            'Domain' : {
+                'Description'   :   'Specifies the domain.',
+                'Required'      :   False,
+                'Value'         :   ''                
+            },
+            'Username' : {
+                'Description'   :   'Specifies the username.',
+                'Required'      :   False,
+                'Value'         :   ''
+            },
+            'Password' : {
+                'Description'   :   'Specifies the password.',
+                'Required'      :   False,
+                'Value'         :   ''
+            },
+            'Cmd' : {
+                'Description'   :   'Specifies the command to execute.',
+                'Required'      :   False,
+                'Value'         :   ''
+            },
+            'NetOnly' : {
+                'Description'   :   'Start the process using the LOGON_NETCREDENTIALS_ONLY flag '
+                                    '(equivalent of running "runas.exe /netonly")',
+                'Required'      :   False,
+                'Value'         :   ''
+            }
+        }
+
+        # save off a copy of the mainMenu object to access external functionality
+        #   like listeners/agent handlers/etc.
+        self.mainMenu = mainMenu
+        
+        for param in params:
+            # parameter format is [Name, Value]
+            option, value = param
+            if option in self.options:
+                self.options[option]['Value'] = value
+
+    def generate(self, obfuscate=False, obfuscationCommand=""):
+        # read in the common module source code
+        moduleSource = self.mainMenu.installPath + "/data/module_source/management/Start-ProcessAsUser.ps1"
+
+        if obfuscate:
+            helpers.obfuscate_module(moduleSource=moduleSource, obfuscationCommand=obfuscationCommand)
+            moduleSource = moduleSource.replace("module_source",
+                                                "obfuscated_module_source")
+
+        try:
+            f = open(moduleSource, 'r')
+        except:
+            print(helpers.color("[!] Could not read module source path at: " +
+                                str(moduleSource)))
+            return ""
+
+        moduleCode = f.read()
+        f.close()
+        script = moduleCode
+        scriptEnd = "\nStart-ProcessAsUser"
+
+        for option, values in self.options.items():
+            if option.lower() != "agent" and option.lower() != "showall":
+                if values['Value'] and values['Value'] != '':
+                    if values['Value'].lower() == "true":
+                        # if we're just adding a switch
+                        scriptEnd += " -" + str(option)
+                    else:
+                        scriptEnd += " -" + str(option) + " " + str(
+                            values['Value'])
+
+        if obfuscate:
+            scriptEnd = helpers.obfuscate(psScript=scriptEnd, obfuscationCommand=obfuscationCommand)
+
+        script += scriptEnd
+        script = helpers.keyword_obfuscation(script)
+
+        return script

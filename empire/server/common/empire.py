907e23232eef060d4783de6e7e944dfd391285d3
Vince Rose <vrose04@gmail.com>
2021-09-19 21:48:44 -0700

    update version

--- empire/server/common/empire.py
+++ empire/server/common/empire.py
@@ -44,7 +44,7 @@ from empire.server.database.base import Session
 from empire.server.database import models
 from sqlalchemy import or_, func, and_
 
-VERSION = "4.1.0 BC Security Fork"
+VERSION = "4.1.1 BC Security Fork"
 
 
 class MainMenu(cmd.Cmd):
